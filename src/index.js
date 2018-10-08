ce075988c628fe6af85491bf2934bb859457b604
cahnory <cahnory@gmail.com>
2018-10-08 10:37:54 +0200

    add build script

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as themeComponent } from './themeComponent';
+
+export { default as CollapsiblePanel } from './components/CollapsiblePanel';
+export { default as DropDownButton } from './components/DropDownButton';
+export { default as PopUpAnchor } from './components/PopUpAnchor';
